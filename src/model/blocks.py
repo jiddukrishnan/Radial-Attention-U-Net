fa8cfea0bf864ad96c3ef1cd382f803fe90eb74b
jiddukrishnan <135655491+jiddukrishnan@users.noreply.github.com>
2025-07-11 01:10:05 +0530

    Update blocks.py

--- src/model/blocks.py
+++ src/model/blocks.py
@@ -1,16 +1,16 @@
 from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, Add, Concatenate
 
-def spiral_convolution(x, filters, spiral_levels=3):
+def radial_convolution(x, filters, radial_levels=3):
     base = Conv2D(filters, (3,3), padding='same')(x)
     base = BatchNormalization()(base)
     base = LeakyReLU(0.1)(base)
-    spiral_features = [base]
-    for i in range(1, spiral_levels+1):
+    radial_features = [base]
+    for i in range(1, radial_levels+1):
         sc = Conv2D(filters, (3,3), padding='same', dilation_rate=(i,i))(base)
         sc = BatchNormalization()(sc)
         sc = LeakyReLU(0.1)(sc)
-        spiral_features.append(sc)
-    fused = Concatenate()(spiral_features)
+        radial_features.append(sc)
+    fused = Concatenate()(radial_features)
     fused = Conv2D(filters, (1,1), padding='same')(fused)
     fused = BatchNormalization()(fused)
     x_proj = Conv2D(filters, (1,1), padding='same')(x)
@@ -20,7 +20,7 @@ def spiral_convolution(x, filters, spiral_levels=3):
     return out
 
  def conv_block(x, filters):
-    x = spiral_convolution(x, filters)
+    x = radial_convolution(x, filters)
     x = Conv2D(filters, (3,3), padding='same')(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.1)(x)
